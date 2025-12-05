b06678c9fa649242b97b4547431abf1ce28970bd
Caleb <caleb@Calebs-MacBook-Pro.local>
2025-12-05 13:08:01 -0500

    Switch back for Render deployment

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///render_temp.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     CORS(app)
